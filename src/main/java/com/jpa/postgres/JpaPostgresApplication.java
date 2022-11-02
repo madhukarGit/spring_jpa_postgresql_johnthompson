67fce1beb9f3dac752cd2e09ba799586586ac870
madhukarGit <hmk02330@gmail.com>
2022-11-02 13:29:51 +0530

    first commit

---
+++ src/main/java/com/jpa/postgres/JpaPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/postgres/JpaPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.postgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
